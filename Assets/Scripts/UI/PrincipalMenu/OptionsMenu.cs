ed3139fce4067d69cc3fa49bb8136ca1dbeefd0d
Ivan <ivan.moratalla.rivera@gmail.com>
2024-11-29 13:17:45 +0100

    Fininish volume settings and starting with brightness

--- Assets/Scripts/UI/PrincipalMenu/OptionsMenu.cs
+++ Assets/Scripts/UI/PrincipalMenu/OptionsMenu.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,7 @@ using UnityEngine.UI;
 public class OptionsMenu : MonoBehaviour
 {
     [SerializeField] private Button goBackButton = null;
+    [SerializeField] private Slider volumeSlider = null;
 
 
     [SerializeField] private MenuHandler menuHandler;
@@ -14,6 +16,9 @@ public class OptionsMenu : MonoBehaviour
     private void Awake()
     {
         goBackButton.onClick.AddListener(GoBack);
+        volumeSlider.onValueChanged.AddListener(SetVolume);
+
+        LoadSettings();
     }
 
     private void GoBack()
@@ -28,5 +33,18 @@ public class OptionsMenu : MonoBehaviour
         {
             this.gameObject.SetActive(false);
         }
+    }
+
+    private void LoadSettings()
+    {
+        // Carga del volumen
+        volumeSlider.value = PlayerPrefs.GetFloat("Volume", 0.5f);
+        AudioListener.volume = volumeSlider.value;
+    }
+
+    public void SetVolume(float volumeValue)
+    {
+        PlayerPrefs.SetFloat("Volume", volumeValue);
+        AudioListener.volume = volumeValue;
     }
 }
