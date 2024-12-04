0519bf96737c631b9e47e37b825bc962733b467a
Ivan <ivan.moratalla.rivera@gmail.com>
2024-12-04 19:09:57 +0100

    Changing rebinding system to accept 2 players

--- Assets/Scripts/Player/InputServiceAsset.cs
+++ Assets/Scripts/Player/InputServiceAsset.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "InputServiceAsset", menuName = "ServiceAssets/InputServiceAsset")]
 public class InputServiceAsset : ScriptableObject
 {
+    [SerializeField] private int playerId;
 
     [SerializeField] private string horizontalAxes;
     [SerializeField] private string verticalAxes;
 
+
+    [Header("Teclas Actuales")]
     [SerializeField] private KeyCode pickObject;
     [SerializeField] private KeyCode releaseObject;
     [SerializeField] private KeyCode cutFood;
     [SerializeField] private KeyCode serveDish;
+
+    [Header("Teclas por Defecto")]
+    [SerializeField] private KeyCode defaultPickObject = KeyCode.E;
+    [SerializeField] private KeyCode defaultReleaseObject = KeyCode.F;
+    [SerializeField] private KeyCode defaultCutFood = KeyCode.C;
+    [SerializeField] private KeyCode defaultServeDish = KeyCode.Q;
 
     private const string PickObjectKey = "PickObjectKey";
     private const string ReleaseObjectKey = "ReleaseObjectKey";
@@ -21,38 +30,37 @@ public class InputServiceAsset : ScriptableObject
 
     private void OnEnable()
     {
-        // Cargar teclas personalizadas (si están guardadas)
-        pickObject = (KeyCode)PlayerPrefs.GetInt(PickObjectKey, (int)pickObject);
-        releaseObject = (KeyCode)PlayerPrefs.GetInt(ReleaseObjectKey, (int)releaseObject);
-        cutFood = (KeyCode)PlayerPrefs.GetInt(CutFoodKey, (int)cutFood);
-        serveDish = (KeyCode)PlayerPrefs.GetInt(ServeDishKey, (int)serveDish);
+        pickObject = (KeyCode)PlayerPrefs.GetInt(GetPrefixedKey(PickObjectKey), (int)defaultPickObject);
+        releaseObject = (KeyCode)PlayerPrefs.GetInt(GetPrefixedKey(ReleaseObjectKey), (int)defaultReleaseObject);
+        cutFood = (KeyCode)PlayerPrefs.GetInt(GetPrefixedKey(CutFoodKey), (int)defaultCutFood);
+        serveDish = (KeyCode)PlayerPrefs.GetInt(GetPrefixedKey(ServeDishKey), (int)defaultServeDish);
     }
 
     public void SetPickObjectKey(KeyCode newKey)
     {
         pickObject = newKey;
-        PlayerPrefs.SetInt(PickObjectKey, (int)newKey);
+        PlayerPrefs.SetInt(GetPrefixedKey(PickObjectKey), (int)newKey);
         PlayerPrefs.Save();
     }
 
     public void SetReleaseObjectKey(KeyCode newKey)
     {
         releaseObject = newKey;
-        PlayerPrefs.SetInt(ReleaseObjectKey, (int)newKey);
+        PlayerPrefs.SetInt(GetPrefixedKey(ReleaseObjectKey), (int)newKey);
         PlayerPrefs.Save();
     }
 
     public void SetCutFoodKey(KeyCode newKey)
     {
         cutFood = newKey;
-        PlayerPrefs.SetInt(CutFoodKey, (int)newKey);
+        PlayerPrefs.SetInt(GetPrefixedKey(CutFoodKey), (int)newKey);
         PlayerPrefs.Save();
     }
 
     public void SetServeDishKey(KeyCode newKey)
     {
         serveDish = newKey;
-        PlayerPrefs.SetInt(ServeDishKey, (int)newKey);
+        PlayerPrefs.SetInt(GetPrefixedKey(ServeDishKey), (int)newKey);
         PlayerPrefs.Save();
     }
 
@@ -66,6 +74,23 @@ public class InputServiceAsset : ScriptableObject
         movementDirection.Normalize();                                                  // Al normalizarlo consigo que en diagonal sea la misma velocidad que al moverse en un solo eje
 
         return movementDirection;
+    }
+
+    public void ResetToDefaults()
+    {
+        // Restablecer valores a los predeterminados
+        pickObject = defaultPickObject;
+        releaseObject = defaultReleaseObject;
+        cutFood = defaultCutFood;
+        serveDish = defaultServeDish;
+
+        // Guardar los valores predeterminados en PlayerPrefs
+        PlayerPrefs.SetInt(GetPrefixedKey(PickObjectKey), (int)defaultPickObject);
+        PlayerPrefs.SetInt(GetPrefixedKey(ReleaseObjectKey), (int)defaultReleaseObject);
+        PlayerPrefs.SetInt(GetPrefixedKey(CutFoodKey), (int)defaultCutFood);
+        PlayerPrefs.SetInt(GetPrefixedKey(ServeDishKey), (int)defaultServeDish);
+
+        PlayerPrefs.Save();
     }
 
     public KeyCode getPickObjectKey()
@@ -86,5 +111,10 @@ public class InputServiceAsset : ScriptableObject
     public KeyCode getServeDishKey()
     {
         return serveDish;
+    }
+
+    private string GetPrefixedKey(string key)
+    {
+        return $"Player{playerId}_{key}";
     }
 }
--- Assets/Scripts/UI/PrincipalMenu/RebindKeyUI.cs
+++ Assets/Scripts/UI/PrincipalMenu/RebindKeyUI.cs
@@ -5,36 +5,57 @@ using UnityEngine.UI;
 
 public class RebindKeyUI : MonoBehaviour
 {
-    [SerializeField] private InputServiceAsset inputServiceAsset;
+    [SerializeField] private InputServiceAsset inputServiceAssetPlayer1;
+    [SerializeField] private InputServiceAsset inputServiceAssetPlayer2;
 
-    [SerializeField] private Button pickObjectButton;
-    [SerializeField] private Button releaseObjectButton;
-    [SerializeField] private Button cutFoodButton;
-    [SerializeField] private Button serveDishButton;
+    // Botones de rebinding para el jugador 1
+    [SerializeField] private Button pickObjectButton_Player1;
+    [SerializeField] private Button releaseObjectButton_Player1;
+    [SerializeField] private Button cutFoodButton_Player1;
+    [SerializeField] private Button serveDishButton_Player1;
+
+    // Botones de rebinding para el jugador 2
+    [SerializeField] private Button pickObjectButton_Player2;
+    [SerializeField] private Button releaseObjectButton_Player2;
+    [SerializeField] private Button cutFoodButton_Player2;
+    [SerializeField] private Button serveDishButton_Player2;
+
+    // Botones para resetear las teclas a los valores por defecto
+    [SerializeField] private Button resetKeysButton_Player1;
+    [SerializeField] private Button resetKeysButton_Player2;
+
 
     private string keyToRebind;
 
     private void Start()
     {
-        // Asignar callbacks a los botones
-        pickObjectButton.onClick.AddListener(() => StartRebind("PickObject", pickObjectButton));
-        releaseObjectButton.onClick.AddListener(() => StartRebind("ReleaseObject", releaseObjectButton));
-        cutFoodButton.onClick.AddListener(() => StartRebind("CutFood", cutFoodButton));
-        serveDishButton.onClick.AddListener(() => StartRebind("ServeDish", serveDishButton));
+        // Asignar callbacks a los botones del jugador 1
+        pickObjectButton_Player1.onClick.AddListener(() => StartRebind("PickObject", inputServiceAssetPlayer1, pickObjectButton_Player1));
+        releaseObjectButton_Player1.onClick.AddListener(() => StartRebind("ReleaseObject", inputServiceAssetPlayer1, releaseObjectButton_Player1));
+        cutFoodButton_Player1.onClick.AddListener(() => StartRebind("CutFood", inputServiceAssetPlayer1, cutFoodButton_Player1));
+        serveDishButton_Player1.onClick.AddListener(() => StartRebind("ServeDish", inputServiceAssetPlayer1, serveDishButton_Player1));
+
+        // Asignar callbacks a los botones del jugador 2
+        pickObjectButton_Player2.onClick.AddListener(() => StartRebind("PickObject", inputServiceAssetPlayer2, pickObjectButton_Player2));
+        releaseObjectButton_Player2.onClick.AddListener(() => StartRebind("ReleaseObject", inputServiceAssetPlayer2, releaseObjectButton_Player2));
+        cutFoodButton_Player2.onClick.AddListener(() => StartRebind("CutFood", inputServiceAssetPlayer2, cutFoodButton_Player2));
+        serveDishButton_Player2.onClick.AddListener(() => StartRebind("ServeDish", inputServiceAssetPlayer2, serveDishButton_Player2));
+
+        // Asignar botones de resetear configuración
+        resetKeysButton_Player1.onClick.AddListener(() => ResetKeys(inputServiceAssetPlayer1));
+        resetKeysButton_Player2.onClick.AddListener(() => ResetKeys(inputServiceAssetPlayer2));
 
         UpdateKeyDisplay();
     }
 
-    private void StartRebind(string action, Button pressedButton)
+    private void StartRebind(string actionToRebind, InputServiceAsset inputServiceAsset, Button pressedButton)
     {
-        keyToRebind = action;
-
         pressedButton.GetComponentInChildren<Text>().text = "Pulse una tecla...";
 
-        StartCoroutine(WaitForKeyPress());
+        StartCoroutine(WaitForKeyPress(actionToRebind, inputServiceAsset));
     }
 
-    private IEnumerator WaitForKeyPress()
+    private IEnumerator WaitForKeyPress(string actionToRebind, InputServiceAsset inputServiceAsset)
     {
         while (!Input.anyKeyDown)                                               // Se espera hasta que el jugador pulse una tecla
         {
@@ -45,7 +66,7 @@ public class RebindKeyUI : MonoBehaviour
         {
             if (Input.GetKeyDown(keyCode))
             {
-                switch (keyToRebind)
+                switch (actionToRebind)
                 {
                     case "PickObject":
                         inputServiceAsset.SetPickObjectKey(keyCode);
@@ -68,12 +89,26 @@ public class RebindKeyUI : MonoBehaviour
         UpdateKeyDisplay();
     }
 
+    private void ResetKeys(InputServiceAsset inputServiceAsset)
+    {
+        inputServiceAsset.ResetToDefaults();
+        UpdateKeyDisplay();
+        Debug.Log($"{inputServiceAsset.name} ha sido restablecido a los valores por defecto.");
+    }
+
     private void UpdateKeyDisplay()
     {
-        pickObjectButton.GetComponentInChildren<Text>().text = inputServiceAsset.getPickObjectKey().ToString();
-        releaseObjectButton.GetComponentInChildren<Text>().text = inputServiceAsset.getReleaseObjectKey().ToString();
-        cutFoodButton.GetComponentInChildren<Text>().text = inputServiceAsset.getCutFoodKey().ToString();
-        serveDishButton.GetComponentInChildren<Text>().text = inputServiceAsset.getServeDishKey().ToString();
+        // Se actualizan las etiquetas de los botones del jugador 1
+        pickObjectButton_Player1.GetComponentInChildren<Text>().text = inputServiceAssetPlayer1.getPickObjectKey().ToString();
+        releaseObjectButton_Player1.GetComponentInChildren<Text>().text = inputServiceAssetPlayer1.getReleaseObjectKey().ToString();
+        cutFoodButton_Player1.GetComponentInChildren<Text>().text = inputServiceAssetPlayer1.getCutFoodKey().ToString();
+        serveDishButton_Player1.GetComponentInChildren<Text>().text = inputServiceAssetPlayer1.getServeDishKey().ToString();
+
+        // Se actualizan las etiquetas de los botones del jugador 2
+        pickObjectButton_Player2.GetComponentInChildren<Text>().text = inputServiceAssetPlayer2.getPickObjectKey().ToString();
+        releaseObjectButton_Player2.GetComponentInChildren<Text>().text = inputServiceAssetPlayer2.getReleaseObjectKey().ToString();
+        cutFoodButton_Player2.GetComponentInChildren<Text>().text = inputServiceAssetPlayer2.getCutFoodKey().ToString();
+        serveDishButton_Player2.GetComponentInChildren<Text>().text = inputServiceAssetPlayer2.getServeDishKey().ToString();
     }
 }
 
