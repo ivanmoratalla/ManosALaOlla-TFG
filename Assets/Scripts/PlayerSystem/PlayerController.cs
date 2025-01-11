92c6018af543950c4e158c04c6342f45759a5984
Ivan <ivan.moratalla.rivera@gmail.com>
2025-01-11 13:26:22 +0100

    Changing players models and adding animations

--- Assets/Scripts/PlayerSystem/PlayerController.cs
+++ Assets/Scripts/PlayerSystem/PlayerController.cs
@@ -11,12 +11,17 @@ public class PlayerController : MonoBehaviour
     // Servicios que utiliza el personaje
     [SerializeField] private MovementServiceAsset movementService;              // Servicio al que se le llamará para mover al personaje
     [SerializeField] private InputServiceAsset inputService;                    // Servicio al que se le llamará para recoger la entrada del usuario y conseguir la dirección de movimiento
+    
+    private Animator animator;
+    private bool isWalking = false;
 
     private Rigidbody rb;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+
         movementService.Initialize(rb);                                                                         // Se inicializa el movimiento (controlo si hay que congelar la rotación para que el personaje no se caiga por las físicas)
     }
 
@@ -25,6 +30,14 @@ public class PlayerController : MonoBehaviour
         Vector3 movementDirection = inputService.Poll();                                                        // Se consigue la dirección de movimiento llamando al servicio encargado de obtenerla
 
         movementService.Move(rb, this.transform, movementDirection);                                            // Se llama al método del servicio que maneja el movimiento
+
+        bool newState = movementDirection.magnitude > 0.1f;
+
+        if(isWalking != newState)                                                                               // Esta comprobación se hace para evitar que se ejecute todo el rato SetBool
+        {
+            isWalking = newState;
+            animator.SetBool("isWalking", isWalking);
+        }
     }
 
 }
